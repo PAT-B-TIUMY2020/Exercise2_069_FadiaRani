0d7656e6121b9d1737474e30b83114eeeb71df8e
Fadia Rani <fadiarani9@gmail.com>
2020-12-25 20:36:25 +0700

    Add project files.

---
+++ ClientRest/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+
+namespace ClientRest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tNIM.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[1].Value);
+            tNama.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+            tProdi.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[2].Value);
+            tAngkatan.Text = Convert.ToString(dataGridView1.Rows[e.RowIndex].Cells[3].Value);
+
+            button4.Enabled = true;
+            button3.Enabled = true;
+            button2.Enabled = false;
+        }
+    }
+}
